313d4600e2a76f767c73569f2c2c3deb4b434072
dli <lideqiang@njust.edu.cn>
2020-12-22 16:39:13 +0800

    update

--- examples/feature_extraction_test.py
+++ examples/feature_extraction_test.py
@@ -5,16 +5,20 @@ from __future__ import print_function
 from core.droidfeature.feature_extraction import Apk2graphs
 from config import config
 
-from tensorflow.compat.v1 import flags
+import argparse
 
-flags.DEFINE_integer('proc_number', 2,
-                     'The number of threads for features extraction')
-flags.DEFINE_integer('number_of_sequences', 200000,
-                     'The maximum number of produced sequences for each app')
-flags.DEFINE_integer('depth_of_recursion', 50,
-                     'The maximum depth restricted on the depth-first traverse')
-flags.DEFINE_integer('time_out', 20,
-                     'The maximum elapsed time for analyzing an app')
+
+cmd_md = argparse.ArgumentParser(description='Argparser for feature extraction')
+cmd_md.add_argument('--proc_number', type=int, default=2,
+                    help='The number of threads for features extraction.')
+cmd_md.add_argument('--number_of_sequences', type=int, default=200000,
+                    help='The maximum number of produced sequences for each app')
+cmd_md.add_argument('--depth_of_recursion', type=int, default=50,
+                    help='The maximum depth restricted on the depth-first traverse')
+cmd_md.add_argument('--time_out', type=int, default=1,
+                    help='The maximum elapsed time for analyzing an app')
+
+args = cmd_md.parse_args()
 
 
 def _main():
@@ -24,16 +28,15 @@ def _main():
     naive_data_saving_dir = config.get('metadata', 'naive_data_pool')
     feature_extractor = Apk2graphs(naive_data_saving_dir,
                                    meta_data_saving_dir,
-                                   number_of_sequences=flags.FLAGS.number_of_sequences,
-                                   depth_of_recursion=flags.FLAGS.depth_of_recursion,
-                                   time_out=flags.FLAGS.time_out,
+                                   number_of_sequences=args.number_of_sequences,
+                                   depth_of_recursion=args.depth_of_recursion,
+                                   time_out=args.time_out,
                                    update=False,
-                                   proc_number=flags.FLAGS.proc_number)
+                                   proc_number=args.proc_number)
     malware_features = feature_extractor.feature_extraction(malware_dir_name)
     print('The number of malware files: ', len(malware_features))
     benign_features = feature_extractor.feature_extraction(benware_dir_name)
     print('The number of benign files: ', len(benign_features))
-
 
 if __name__ == '__main__':
     _main()
