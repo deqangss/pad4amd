1e5a7aca71d8e32a2e89e2681d810dc67b5e9021
dli <lideqiang@njust.edu.cn>
2021-01-02 10:09:00 +0800

    update

--- core/defense/malgat.py
+++ core/defense/malgat.py
@@ -106,13 +106,15 @@ class MalGAT(nn.Module):
             features = features.to_sparse()
             features = torch.cat([header(features, adj) for header in headers], dim=-1)
 
-        latent_codes = x.unsqueeze(-1) * features
-        # latent codes shape --> [self.k, batch_size, vocab_size, vocab_dim]
-        # --> [self.k, batch_size, vocab_dim] (global max pooling)
-        # --> [self.k+1, batch_size, vocab_dim] (insert the 'cls' codes)
-        # --> [batch_size, self.k+1, vocab_dim] (permutation)
-        latent_codes = torch.max(latent_codes, dim=-2)[0]
-        latent_codes = torch.vstack([self.cls_weight.view(1, 1, -1).repeat(1, x[0].size()[0], 1), latent_codes]).permute([1,0,2])
+        latent_codes = [torch.stack([self.cls_weight] * x[0].size()[0])]
+        for i in range(self.k):
+            latent_code = torch.unsqueeze(x[i],
+                                          dim=-1) * features  # masking out the unused representations via broadcasting, herein the latent_code shape is [batch_size, vocab_size, feature_dim]
+            latent_code, _1 = torch.max(latent_code,
+                                        dim=1)  # after max pooling, the latent_code shape is [batch_size, feature_dim]
+            latent_codes.append(latent_code)
+
+        latent_codes = torch.stack(latent_codes, dim=1)  # the result shape is [batch_size, self.k+1, feature_dim]
         latent_codes = self.cls_attn_layer(latent_codes)
         latent_codes = self.activation(self.dense(latent_codes))
         return latent_codes
