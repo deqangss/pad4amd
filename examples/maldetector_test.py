0fc7956806ebcc44637641fdf66b17da67d23b0b
dli <lideqiang@njust.edu.cn>
2021-01-10 23:40:04 +0800

    update

--- examples/maldetector_test.py
+++ examples/maldetector_test.py
@@ -63,7 +63,7 @@ def _main():
     val_data, valy = dataset.validation_dataset
     test_data, testy = dataset.test_dataset
     train_dataset_producer = dataset.get_input_producer(train_data, trainy, batch_size=args.batch_size, name='train')
-    val_dataset_producer = dataset.get_input_producer(val_data, valy, batch_size=2 * args.batch_size, name='val')
+    val_dataset_producer = dataset.get_input_producer(val_data, valy, batch_size=args.batch_size, name='val')
     test_dataset_producer = dataset.get_input_producer(test_data, testy, batch_size=args.batch_size, name='test')
     assert dataset.n_classes == 2
 
