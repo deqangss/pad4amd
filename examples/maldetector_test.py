d678a4b6ed0acf49c64e4c12622af91e3123c7e4
dli <lideqiang@njust.edu.cn>
2021-01-12 00:15:57 +0800

    update

--- examples/maldetector_test.py
+++ examples/maldetector_test.py
@@ -38,7 +38,7 @@ detector_argparse = cmd_md.add_argument_group(title='detector')
 detector_argparse.add_argument('--cuda', action='store_true', default=False, help='whether use cuda enable gpu or cpu.')
 detector_argparse.add_argument('--seed', type=int, default=0, help='random seed.')
 detector_argparse.add_argument('--embedding_dim', type=int, default=8, help='embedding dimension')
-detector_argparse.add_argument('--hidden_units', type=lambda s: [int(u) for u in s.split(',')], default='32',
+detector_argparse.add_argument('--hidden_units', type=lambda s: [int(u) for u in s.split(',')], default='16',
                                help='delimited list input, e.g., "32,32"',)
 detector_argparse.add_argument('--penultimate_hidden_unit', type=int, default=64, help='dimension of penultimate layer')
 detector_argparse.add_argument('--n_heads', type=int, default=2, help='number of headers')
@@ -48,7 +48,7 @@ detector_argparse.add_argument('--n_sample_times', type=int, default=5, help='ti
 detector_argparse.add_argument('--alpha', type=float, default=0.2, help='slope coefficient of leaky-relu')
 detector_argparse.add_argument('--sparse', action='store_true', default=True, help='GAT with sparse version or not.')
 
-detector_argparse.add_argument('--batch_size', type=int, default=8, help='minibatch size')
+detector_argparse.add_argument('--batch_size', type=int, default=16, help='minibatch size')
 detector_argparse.add_argument('--epochs', type=int, default=10, help='number of epochs to train.')
 detector_argparse.add_argument('--lr', type=float, default=0.005, help='initial learning rate.')
 detector_argparse.add_argument('--patience', type=int, default=100, help='patience')
