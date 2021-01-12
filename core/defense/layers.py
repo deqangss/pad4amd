e217a05927e00e3cb8c9ed963fc94cecd55d6cce
dli <lideqiang@njust.edu.cn>
2021-01-12 14:02:14 +0800

    update

--- core/defense/layers.py
+++ core/defense/layers.py
@@ -193,7 +193,7 @@ class SpGraphAttentionLayer(nn.Module):
         # edge_e: E
 
         sp_edge_e = torch.sparse_coo_tensor(edge, edge_e, torch.Size([batch_size, N, N]))
-        if self.training or self.adv_testing:
+        if self.training or self.adv_testing or sp_edge_e._nnz() > 10000:
             e_rowsum = torch.stack(
                 [torch.sparse.mm(sp_e, torch.ones(size=(N, 1), dtype=torch.float, device=dv)) for sp_e in sp_edge_e])
         else:
