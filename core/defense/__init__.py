f8118d556b1ee1642b068ed3b5527b176b6f0aec
dli <lideqiang@njust.edu.cn>
2021-01-15 20:30:05 +0800

    update

--- core/defense/__init__.py
+++ core/defense/__init__.py
@@ -1,2 +1,3 @@
 from core.defense.dataset import Dataset
-from core.defense.maldetector import MalwareDetector
+from core.defense.maldet import MalwareDetector
+from core.defense.maldet_advdicator import MalwareDetectorIndicator
