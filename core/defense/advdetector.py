7295751dcdb2bdd7e478b603e444303c47c45b71
dli <lideqiang@njust.edu.cn>
2021-01-16 11:55:12 +0800

    update

--- core/defense/advdetector.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-from core.defense.maldetector import MalwareDetector, MalGAT
-
-
-class AdversarialMalwareDetector(MalwareDetector):
-    raise NotImplementedError
