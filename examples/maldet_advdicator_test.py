2a9fd63e809f77b894dc7f937c496e9a8be10deb
dli <lideqiang@njust.edu.cn>
2021-01-30 12:59:26 +0800

    update

--- examples/maldet_advdicator_test.py
+++ examples/maldet_advdicator_test.py
@@ -45,12 +45,12 @@ def _main():
                                      n_classes=dataset.n_classes,
                                      device=dv,
                                      sample_weights=dataset.sample_weights,
-                                     name='20210130-102323',
+                                     name=time.strftime("%Y%m%d-%H%M%S"),
                                      **vars(args)
                                      )
     model = model.to(dv)
     save_args(path.join(path.dirname(model.model_save_path), "hparam"), vars(args))
-    model.load_state_dict(torch.load(model.model_save_path))
+    # model.load_state_dict(torch.load(model.model_save_path))
     model.fit(train_dataset_producer,
               val_dataset_producer,
               epochs=args.epochs,
