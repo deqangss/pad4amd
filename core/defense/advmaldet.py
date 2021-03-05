9a146a6eecaacd4f1b6c34f0a9f24ffc8c91a5c4
dli <lideqiang@njust.edu.cn>
2021-03-05 10:23:11 +0800

    update

--- core/defense/advmaldet.py
+++ core/defense/advmaldet.py
@@ -190,10 +190,10 @@ class MalwareDetectorIndicator(MalwareDetector):
         gamma_z = torch.softmax(logits, dim=1)
         prob_n = self.gaussian_prob(representation)
 
-        print(prob_n)
+        # print(prob_n)
         # print(self.phi)
         # print(self.sample_weights)
-        # print(torch.sum(prob_n * self.phi + exp_over_flow, dim=1))
+        print(torch.sum(prob_n * self.phi + exp_over_flow, dim=1))
         # print(torch.sum(-torch.log(prob_n * self.phi + exp_over_flow), dim=1))
         # E_z = torch.sum(torch.log(prob_n * self.phi + exp_over_flow) * self.sample_weights, dim=1)
         E_z = torch.sum(gamma_z * torch.log(prob_n * self.phi / gamma_z + exp_over_flow) * self.sample_weights, dim=1)  # ELBO
