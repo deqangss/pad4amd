8cd025638965554557c4c15733ac66e023c273cc
dli <lideqiang@njust.edu.cn>
2021-03-15 21:50:31 +0800

    update

--- core/defense/__init__.py
+++ core/defense/__init__.py
@@ -2,3 +2,4 @@ from core.defense.dataset import Dataset
 from core.defense.maldet import MalwareDetector
 from core.defense.advdet_gmm import MalwareDetectorIndicator, EXP_OVER_FLOW
 from core.defense.principled_adv_training import PrincipledAdvTraining
+from core.defense.advdet_kde import KernelDensityEstimation
