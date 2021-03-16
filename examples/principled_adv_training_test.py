5602bee6dfa4059ae207ded842f2677257d614a1
dli <lideqiang@njust.edu.cn>
2021-03-16 17:41:19 +0800

    update

--- examples/principled_adv_training_test.py
+++ examples/principled_adv_training_test.py
@@ -77,6 +77,7 @@ def _main():
         # print(principled_adv_training_model.model.tau)
         # principled_adv_training_model.model.save_to_disk()
     # test: accuracy
+    principled_adv_training_model.model.load()
     principled_adv_training_model.model.predict(test_dataset_producer, use_indicator=False)
 
 
