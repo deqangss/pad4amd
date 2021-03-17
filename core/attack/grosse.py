15001a21734721cbe771ab54577a936b1576569c
dli <lideqiang@njust.edu.cn>
2021-03-17 17:19:24 +0800

    add grosse attack

---
+++ core/attack/grosse.py
@@ -0,0 +1,142 @@
+"""
+@inproceedings{grosse2017adversarial,
+  title={Adversarial examples for malware detection},
+  author={Grosse, Kathrin and Papernot, Nicolas and Manoharan, Praveen and Backes, Michael and McDaniel, Patrick},
+  booktitle={European symposium on research in computer security},
+  pages={62--79},
+  year={2017},
+  organization={Springer}
+}
+"""
+
+import torch
+import torch.nn.functional as F
+
+from core.attack.base_attack import BaseAttack
+from tools.utils import rand_x
+from config import logging, ErrorHandler
+
+logger = logging.getLogger('core.attack.grosse')
+logger.addHandler(ErrorHandler)
+
+EXP_OVER_FLOW = -30
+
+
+class Groose(BaseAttack):
+    """
+    Multi-step bit coordinate ascent using softmax output (rather than the loss)
+
+    Parameters
+    ---------
+    @param kappa, attack confidence
+    @param manipulation_z, manipulations
+    @param omega, the indices of interdependent apis corresponding to each api
+    @param device, 'cpu' or 'cuda'
+    """
+
+    def __init__(self, kappa=10., manipulation_z=None, omega=None, device=None):
+        super(Groose, self).__init__(manipulation_z, omega, device)
+        self.omega = None  # no interdependent apis if just api insertion is considered
+        self.manipulation_z = None  # all apis are insertable
+        self.kappa = kappa
+        self.lambda_ = 1.
+
+    def perturb(self, model, x, adj=None, label=None,
+                m_perturbations=10,
+                lambda_=1.,
+                use_sample=False,
+                stop=True,
+                verbose=False):
+        """
+        perturb node feature vectors
+
+        Parameters
+        -----------
+        @param model, a victim model
+        @param x: torch.FloatTensor, node feature vectors (each represents the occurrences of apis in a graph) with shape [batch_size, number_of_graphs, vocab_dim]
+        @param adj: torch.FloatTensor or None, adjacency matrix (if not None, the shape is [number_of_graphs, batch_size, vocab_dim, vocab_dim])
+        @param label: torch.LongTensor, ground truth labels
+        @param m_perturbations: Integer, maximum number of perturbations
+        @param lambda_, float, penalty factor
+        @param use_sample, Boolean, whether use random start point
+        @param stop, Boolean, whether stop once evade victim successfully
+        @param verbose, Boolean, whether present attack information or not
+        """
+        if x is None and x.shape[0] <= 0:
+            return []
+        adv_x = x.detach().clone().to(torch.float)
+        self.lambda_ = lambda_
+        padding_mask = torch.sum(adv_x, dim=-1, keepdim=True) > 1  # we set a graph contains two apis at least
+        model.eval()
+        for iter_i in range(m_perturbations):
+            if use_sample and iter_i == 0:
+                adv_x = rand_x(adv_x, is_sample=True)
+            var_adv_x = torch.autograd.Variable(adv_x, requires_grad=True)
+            hidden, logit = model.forward(var_adv_x, adj)
+            loss, done = self.get_losses(model, logit, hidden)
+            if verbose:
+                print(
+                    f"\n Iteration {iter_i}: the accuracy is {(logit.argmax(1) == 1.).sum().item() / adv_x.size()[0] * 100:.3f}.")
+            if torch.all(done) and stop:
+                break
+            grad = torch.autograd.grad(torch.mean(loss), var_adv_x)[0].data
+
+            # filtering un-considered graphs & positions
+            grad = grad * padding_mask
+            grad4insertion = (grad > 0) * grad * (adv_x < 0.5)
+
+            grad4ins_ = grad4insertion.reshape(x.shape[0], -1)
+            _, pos = torch.max(grad4ins_, dim=-1)
+            perturbation = F.one_hot(pos, num_classes=grad4ins_.shape[-1]).float().reshape(x.shape)
+            if stop:
+                perturbation[done] = 0.
+            adv_x = torch.clamp(adv_x + perturbation, min=0., max=1.)
+        return adv_x
+
+    def perturb_ehs(self, model, x, adj=None, label=None,
+                    m_perturbations=10,
+                    min_lambda_=1e-5,
+                    max_lambda_=1e5,
+                    use_sample=False,
+                    stop=True,
+                    verbose=False):
+        """
+        enhance attack
+        """
+        assert 0 < min_lambda_ <= max_lambda_
+        self.lambda_ = min_lambda_
+        adv_node = x.detach().clone()
+        while self.lambda_ <= max_lambda_:
+            hidden, logit = model.forward(adv_node, adj)
+            _, done = self.get_losses(model, logit, hidden)
+            if verbose:
+                logger.info(
+                    f"BCA attack: attack effectiveness {done.sum().item() / x.size()[0]} with lambda {self.lambda_}.")
+            if torch.all(done):
+                return adv_node
+
+            adv_adj = None if adj is None else adv_adj[~done]
+            pert_x = self.perturb(model, adv_node[~done], adv_adj, label[~done],
+                                  m_perturbations,
+                                  lambda_=self.lambda_,
+                                  use_sample=use_sample,
+                                  stop=stop,
+                                  verbose=False
+                                  )
+            adv_node[~done] = pert_x
+            self.lambda_ *= 10.
+        return adv_node
+
+    def get_losses(self, model, logit, hidden=None):
+        softmax_loss = torch.softmax(logit, dim=-1)[:, 0]
+        if 'forward_g' in type(model).__dict__.keys():
+            de = model.forward_g(hidden, logit.argmax(1))
+            tau = model.get_tau_sample_wise(logit.argmax(1))
+            loss_no_reduction = softmax_loss + \
+                                self.lambda_ * (torch.clamp(
+                torch.log(de + EXP_OVER_FLOW) - torch.log(tau + EXP_OVER_FLOW), max=self.kappa))
+            done = (logit.argmax(1) == 0.) & (de >= tau)
+        else:
+            loss_no_reduction = softmax_loss
+            done = logit.argmax(1) == 0.
+        return loss_no_reduction, done
---
+++ examples/grosse_test.py
@@ -0,0 +1,143 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import os
+from tqdm import tqdm
+import argparse
+
+import numpy as np
+
+from core.defense import Dataset
+from core.defense import MalwareDetector, MalwareDetectorIndicator, PrincipledAdvTraining, KernelDensityEstimation
+from core.attack import Groose
+from tools import utils
+from config import config, logging, ErrorHandler
+
+logger = logging.getLogger('examples.grosse_test')
+logger.addHandler(ErrorHandler)
+
+ompa_argparse = argparse.ArgumentParser(description='arguments for orthogonal matching pursuit attack')
+ompa_argparse.add_argument('--lambda_', type=float, default=0.01, help='balance factor for waging attack.')
+ompa_argparse.add_argument('--n_pertb', type=int, default=100, help='maximum number of perturbations.')
+ompa_argparse.add_argument('--kappa', type=float, default=10., help='attack confidence.')
+ompa_argparse.add_argument('--ascending', action='store_true', default=False,
+                           help='whether start the perturbations gradually.')
+ompa_argparse.add_argument('--n_sample_times', type=int, default=1, help='sample times for producing data.')
+ompa_argparse.add_argument('--kde', action='store_true', default=False, help='incorporate kernel density estimation.')
+ompa_argparse.add_argument('--model', type=str, default='maldet',
+                           choices=['maldet', 'advmaldet', 'prip_adv'],
+                           help="model type, either of 'maldet', 'advmaldet' and 'prip_adv'.")
+ompa_argparse.add_argument('--model_name', type=str, default='pro', help='model name.')
+
+
+def _main():
+    args = ompa_argparse.parse_args()
+    if args.model == 'maldet':
+        save_dir = config.get('experiments', 'malware_detector') + '_' + args.model_name
+    elif args.model == 'advmaldet':
+        save_dir = config.get('experiments', 'malware_detector_indicator') + '_' + args.model_name
+    elif args.model == 'prip_adv':
+        save_dir = config.get('experiments', 'prip_adv_training') + '_' + args.model_name
+    else:
+        raise TypeError("Expected 'maldet', 'advmaldet' or 'prip_adv'.")
+
+    hp_params = utils.read_pickle(os.path.join(save_dir, 'hparam.pkl'))
+    dataset = Dataset(hp_params['dataset_name'],
+                      k=hp_params['k'],
+                      use_cache=False,
+                      is_adj=hp_params['is_adj'],
+                      feature_ext_args={'proc_number': hp_params['proc_number']}
+                      )
+    test_x, testy = dataset.test_dataset
+    mal_test_x = test_x[testy == 1]
+    mal_testy = testy[testy == 1]
+    mal_count = len(mal_testy)
+    if mal_count <= 0:
+        return
+    mal_test_dataset_producer = dataset.get_input_producer(mal_test_x, mal_testy,
+                                                           batch_size=hp_params['batch_size'],
+                                                           name='test')
+    assert dataset.n_classes == 2
+
+    # test
+    if not hp_params['cuda']:
+        dv = 'cpu'
+    else:
+        dv = 'cuda'
+    if args.model == 'maldet':
+        model = MalwareDetector(dataset.vocab_size,
+                                dataset.n_classes,
+                                device=dv,
+                                name=args.model_name,
+                                **hp_params
+                                )
+    else:
+        model = MalwareDetectorIndicator(vocab_size=dataset.vocab_size,
+                                         n_classes=dataset.n_classes,
+                                         device=dv,
+                                         sample_weights=dataset.sample_weights,
+                                         name=args.model_name,
+                                         **hp_params
+                                         )
+    model = model.to(dv)
+    if args.model == 'prip_adv':
+        PrincipledAdvTraining(model)
+    if args.kde:
+        save_dir = config.get('experiments', 'kde') + '_' + args.model_name
+        hp_params = utils.read_pickle(os.path.join(save_dir, 'hparam.pkl'))
+        model = KernelDensityEstimation(model,
+                                        n_centers=hp_params['n_centers'],
+                                        bandwidth=hp_params['bandwidth'],
+                                        n_classes=dataset.n_classes,
+                                        ratio=hp_params['ratio']
+                                        )
+
+    model.load()
+    print("Load model parameters from {}.".format(model.model_save_path))
+    attack = Groose(device=model.device,
+                    kappa=args.kappa
+                    )
+    # test: accuracy
+    if args.ascending:
+        interval = 10
+    else:
+        interval = args.n_pertb
+    for m in range(interval, args.n_pertb + 1, interval):
+        logger.info("\nThe maximum number of perturbations for each example is {}:".format(m))
+        prist_acc = []
+        adv_acc = []
+        prist_acc_ = []
+        adv_acc_ = []
+        model.eval()
+        for i in range(args.n_sample_times):
+            prist_preds = []
+            adv_preds = []
+            prist_preds_ = []
+            adv_preds_ = []
+            for res in mal_test_dataset_producer:
+                x_batch, adj, y_batch = res
+                x_batch, adj_batch, y_batch = utils.to_tensor(x_batch, adj, y_batch, model.device)
+                adv_x_batch = attack.perturb(model, x_batch, adj_batch, y_batch, m, args.lambda_,
+                                             use_sample=False,
+                                             verbose=True)
+
+                prist_preds.append(model.inference_batch_wise(x_batch, adj, y_batch, use_indicator=False))
+                adv_preds.append(model.inference_batch_wise(adv_x_batch, adj, y_batch, use_indicator=False))
+                prist_preds_.append(model.inference_batch_wise(x_batch, adj, y_batch, use_indicator=True))
+                adv_preds_.append(model.inference_batch_wise(adv_x_batch, adj, y_batch, use_indicator=True))
+            adv_acc.append(np.mean(np.concatenate(adv_preds)))
+            prist_acc.append(np.mean(np.concatenate(prist_preds)))
+            adv_acc_.append(np.mean(np.concatenate(adv_preds_)))
+            prist_acc_.append(np.mean(np.concatenate(prist_preds_)))
+            logger.info(
+                f'Sampling {i + 1}: accuracy on pristine vs. adversarial malware is {prist_acc[-1] * 100:.3f}% vs. {adv_acc[-1] * 100:.3f}%.')
+            logger.info(
+                f'Sampling {i + 1} (W/ indicator): accuracy on pristine vs. adversarial malware is {prist_acc_[-1] * 100:.3f}% vs. {adv_acc_[-1] * 100:.3f}%.')
+        logger.info(
+            f'The mean accuracy on pristine vs. adversarial malware is {sum(prist_acc) / args.n_sample_times * 100:.3f}% vs. {sum(adv_acc) / args.n_sample_times * 100:.3f}%.')
+        logger.info(
+            f'The mean accuracy on pristine vs. adversarial malware is (W/ indicator) {sum(prist_acc_) / args.n_sample_times * 100:.3f}% vs. {sum(adv_acc_) / args.n_sample_times * 100:.3f}%.')
+
+
+if __name__ == '__main__':
+    _main()
