40f199f8083525eee6fcd9f26e9b29bd66b69ac3
dli <lideqiang@njust.edu.cn>
2021-03-29 18:31:45 +0800

    update

--- examples/pgdl1_test.py
+++ examples/pgdl1_test.py
@@ -18,11 +18,14 @@ logger.addHandler(ErrorHandler)
 atta_argparse = argparse.ArgumentParser(description='arguments for l1 norm based projected gradient descent attack')
 atta_argparse.add_argument('--lambda_', type=float, default=0.01, help='balance factor for waging attack.')
 atta_argparse.add_argument('--m_pertb', type=int, default=100, help='maximum number of perturbations.')
-atta_argparse.add_argument('--kappa', type=float, default=10., help='attack confidence.')
-atta_argparse.add_argument('--kde', action='store_true', default=False, help='attack model with kernel density estimation.')
+atta_argparse.add_argument('--base', type=float, default=10., help='base of a logarithm function.')
+atta_argparse.add_argument('--kappa', type=float, default=1., help='attack confidence.')
+atta_argparse.add_argument('--real', action='store_true', default=True, help='whether produce the perturbed apks.')
+atta_argparse.add_argument('--kde', action='store_true', default=False,
+                           help='attack model with kernel density estimation.')
 atta_argparse.add_argument('--model', type=str, default='maldet',
-                           choices=['maldet', 'advmaldet', 'p_adv_train'],
-                           help="model type, either of 'maldet', 'advmaldet' and 'p_adv_train'.")
+                           choices=['maldet', 'advmaldet', 'padvtrain'],
+                           help="model type, either of 'maldet', 'advmaldet' and 'padvtrain'.")
 atta_argparse.add_argument('--model_name', type=str, default='xxxxxxxx-xxxxxx', help='model timestamp.')
 
 
@@ -32,10 +35,10 @@ def _main():
         save_dir = config.get('experiments', 'malware_detector') + '_' + args.model_name
     elif args.model == 'advmaldet':
         save_dir = config.get('experiments', 'malware_detector_indicator') + '_' + args.model_name
-    elif args.model == 'p_adv_train':
+    elif args.model == 'padvtrain':
         save_dir = config.get('experiments', 'p_adv_training') + '_' + args.model_name
     else:
-        raise TypeError("Expected 'maldet', 'advmaldet' or 'p_adv_train'.")
+        raise TypeError("Expected 'maldet', 'advmaldet' or 'padvtrain'.")
 
     hp_params = utils.read_pickle(os.path.join(save_dir, 'hparam.pkl'))
     dataset = Dataset(hp_params['dataset_name'],
@@ -74,7 +77,7 @@ def _main():
                                          **hp_params
                                          )
     model = model.to(dv)
-    if args.model == 'p_adv_train':
+    if args.model == 'padvtrain':
         PrincipledAdvTraining(model)
     if args.kde:
         save_dir = config.get('experiments', 'kde') + '_' + args.model_name
@@ -87,31 +90,33 @@ def _main():
                                         )
 
     model.load()
-    print("Load model parameters from {}.".format(model.model_save_path))
-    model.predict(mal_test_dataset_producer, use_indicator=False)
-    model.predict(mal_test_dataset_producer, use_indicator=True)
+    logger.info("Load model parameters from {}.".format(model.model_save_path))
+    # model.predict(mal_test_dataset_producer)
 
-    attack = PGDl1(device=model.device,
-                   kappa=args.kappa
-                   )
+    attack = PGDl1(kappa=args.kappa, device=model.device)
 
     logger.info("\nThe maximum number of perturbations for each example is {}:".format(args.m_pertb))
     y_cent_list, x_density_list = [], []
+    x_mod_integrated = []
     model.eval()
     for i in range(hp_params['n_sample_times']):
         y_cent, x_density = [], []
-        for x, a, y in mal_test_dataset_producer:
+        x_mod = []
+        for x, a, y, g_ind in mal_test_dataset_producer:
             x, a, y = utils.to_tensor(x, a, y, model.device)
             adv_x_batch = attack.perturb(model, x, a, y,
                                          args.m_pertb,
-                                         args.lambda_,
+                                         min_lambda_=1e-5,
+                                         max_lambda_=1e5,
+                                         base=args.base,
                                          verbose=True)
             y_cent_batch, x_density_batch = model.inference_batch_wise(adv_x_batch, a, y, use_indicator=True)
             y_cent.append(y_cent_batch)
             x_density.append(x_density_batch)
+            x_mod.extend(dataset.get_modification(adv_x_batch, x, g_ind, True))
         y_cent_list.append(np.vstack(y_cent))
         x_density_list.append(np.concatenate(x_density))
-
+        x_mod_integrated = dataset.modification_integ(x_mod_integrated, x_mod)
     y_cent = np.mean(np.stack(y_cent_list, axis=1), axis=1)
     y_pred = np.argmax(y_cent, axis=-1)
     logger.info(
@@ -122,6 +127,12 @@ def _main():
         logger.info(f"The effectiveness of indicator is {sum(~indicator_flag) / mal_count * 100:.3f}%")
         acc_w_indicator = (sum(~indicator_flag) + sum((y_pred == 1.) & indicator_flag)) / mal_count * 100
         logger.info(f'The mean accuracy on adversarial malware (w/ indicator) is {acc_w_indicator:.3f}%.')
+
+    if args.real:
+        attack.produce_adv_mal(x_mod_integrated, mal_test_x.tolist(),
+                               config.get('dataset', 'malware_dir'),
+                               adj_mod=None)
+
 
 if __name__ == '__main__':
     _main()
