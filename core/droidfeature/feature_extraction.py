d1d9cc4ad6d2fdb4651f788603878dd5647d4f18
dli <lideqiang@njust.edu.cn>
2021-04-12 18:42:42 +0800

    update

--- core/droidfeature/feature_extraction.py
+++ core/droidfeature/feature_extraction.py
@@ -300,7 +300,7 @@ class Apk2graphs(object):
             cache_feature_path = os.path.join(cache_dir, os.path.basename(feature_path))
         if cache_feature_path is not None and os.path.exists(cache_feature_path):
             # return utils.read_pickle(cache_feature_path)
-            return cPickle.load(cache_feature_path)
+            return cPickle.load(open(cache_feature_path, 'rb'))
 
         cg_dict = seq_gen.read_from_disk(feature_path)
         sub_features = []
@@ -317,7 +317,8 @@ class Apk2graphs(object):
 
         if cache_feature_path is not None:
             # utils.dump_pickle((sub_features, sub_adjs, label), cache_feature_path)
-            cPickle.dump((sub_features, sub_adjs, label), cache_feature_path)
+            with open(cache_feature_path, 'wb') as wr:
+                cPickle.dump((sub_features, sub_adjs, label), wr)
         return sub_features, sub_adjs, label
 
         # numerical_representation_dict = collections.defaultdict(tuple)
