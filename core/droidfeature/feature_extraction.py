a66badb96c4402f0fb1107e53b0cf9ac63f569d7
dli <lideqiang@njust.edu.cn>
2021-04-12 18:32:08 +0800

    update

--- core/droidfeature/feature_extraction.py
+++ core/droidfeature/feature_extraction.py
@@ -4,6 +4,7 @@ import multiprocessing
 
 import collections
 import numpy as np
+import torch
 from scipy.sparse import csr_matrix
 import networkx as nx
 import itertools
@@ -298,7 +299,8 @@ class Apk2graphs(object):
         if cache_dir is not None:
             cache_feature_path = os.path.join(cache_dir, os.path.basename(feature_path))
         if cache_feature_path is not None and os.path.exists(cache_feature_path):
-            return utils.read_pickle(cache_feature_path)
+            # return utils.read_pickle(cache_feature_path)
+            return torch.load(cache_feature_path)
 
         cg_dict = seq_gen.read_from_disk(feature_path)
         sub_features = []
@@ -314,7 +316,8 @@ class Apk2graphs(object):
                 break
 
         if cache_feature_path is not None:
-            utils.dump_pickle((sub_features, sub_adjs, label), cache_feature_path)
+            # utils.dump_pickle((sub_features, sub_adjs, label), cache_feature_path)
+            torch.save((sub_features, sub_adjs, label), cache_feature_path)
         return sub_features, sub_adjs, label
 
         # numerical_representation_dict = collections.defaultdict(tuple)
