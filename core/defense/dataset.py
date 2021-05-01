4d9538293e71d7c4b0f1ad19ab74ad78b1c254b3
dli <lideqiang@njust.edu.cn>
2021-05-01 20:19:31 +0800

    update

--- core/defense/dataset.py
+++ core/defense/dataset.py
@@ -78,7 +78,9 @@ class Dataset(torch.utils.data.Dataset):
 
         # self.feature_extractor.merge_cg(self.train_dataset[0])
         # self.feature_extractor.merge_cg(self.validation_dataset[0])
-        # self.feature_extractor.merge_cg(self.test_dataset[0])
+        self.feature_extractor.merge_cg(self.test_dataset[0])
+        import sys
+        sys.exit(1)
 
         self.vocab, _1, flag = self.feature_extractor.get_vocab(*self.train_dataset)
         self.vocab_size = len(self.vocab)
