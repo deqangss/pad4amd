eee77c9f8d911f3c2b74b474037aa03f07ed2c71
dli <lideqiang@njust.edu.cn>
2021-05-10 22:29:30 +0800

    update

--- examples/pgd_test.py
+++ examples/pgd_test.py
@@ -129,6 +129,7 @@ def _main():
     attack = PGD(norm=args.norm,
                  use_random=args.random_start,
                  rounding_threshold=args.round_threshold,
+                 is_attacker=False,
                  oblivion=args.oblivion,
                  kappa=args.kappa,
                  device=model.device
