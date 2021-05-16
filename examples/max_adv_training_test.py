c6a84bc6dca814aa3365a700b0638a47c05a2612
dli <lideqiang@njust.edu.cn>
2021-05-17 00:45:26 +0800

    update

--- examples/max_adv_training_test.py
+++ examples/max_adv_training_test.py
@@ -109,7 +109,7 @@ def _main():
                               verbose=False
                               )
 
-    attack = Max(attack_list=[pgdlinf],
+    attack = Max(attack_list=[ompap, pgdlinf, pgdadma],
                  varepsilon=1e-9,
                  is_attacker=False,
                  device=model.device
