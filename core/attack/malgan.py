4e90e72ec9a455bb05a122a634836b84a420423b
dli <lideqiang@njust.edu.cn>
2021-05-19 17:59:02 +0800

    update

--- core/attack/malgan.py
+++ core/attack/malgan.py
@@ -54,9 +54,9 @@ class MalGAN(BaseAttack, nn.Module):
             return layers
 
         self.generator = nn.Sequential(
-            *block(self.latent_dim, 128, normalize=False),
-            *block(128, 512, normalize=False),
-            *block(512, 1024, normalize=False),
+            *block(self.latent_dim, 128, normalize=True),
+            *block(128, 512, normalize=True),
+            *block(512, 1024, normalize=True),
             nn.Linear(1024, self.input_dim),
             nn.Sigmoid()
         )
