5039e01a42c310112815554507c201f73618339a
dli <lideqiang@njust.edu.cn>
2021-05-26 17:42:17 +0800

    update

--- examples/max_test.py
+++ examples/max_test.py
@@ -281,11 +281,16 @@ def _main():
                                                                batch_size=hp_params['batch_size'],
                                                                name='test'
                                                                )
+
         y_pred2, indicator_flag2 = model.predict(ben_test_dataset_producer)
-        pred_label = (~indicator_flag) | ((y_pred == 1.) & indicator_flag).tolist()
         pred_label2 = (~indicator_flag2) | ((y_pred2 == 1.) & indicator_flag2).tolist()
-        for idx, p1, p2, x_mod, mal_name in enumerate(zip(pred_label, pred_label2, x_mod_integrated, mal_test_x.tolist())):
-            print(idx, mal_name, p1, p2)
+        pred_label = (~indicator_flag) | ((y_pred == 1.) & indicator_flag).tolist()
+        for idx, adv_fname, p2 in enumerate(zip(adv_feature_paths, pred_label2)):
+            fname = adv_fname.split('_')[0] + '.gpickle'
+            idx = mal_test_x.tolist().index(fname)
+            x_mod = x_mod_integrated[idx]
+            p1 = pred_label[idx]
+            print(idx, fname, p1, p2)
             print(torch.sum(x_mod.to_dense() > 0))
             print(torch.sum(x_mod.to_dense() < 0))
 
