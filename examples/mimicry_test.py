83772bfe178486cfa655840586efd95cf1e88d9b
dli <lideqiang@njust.edu.cn>
2021-05-29 13:48:00 +0800

    update

--- examples/mimicry_test.py
+++ examples/mimicry_test.py
@@ -143,6 +143,7 @@ def _main():
                                 os.path.join(save_dir, 'x_mod.list'))
     if args.real:
         adv_app_dir = os.path.join(save_dir, 'adv_apps')
+        print(len(x_mod_list))
         attack.produce_adv_mal(x_mod_list, mal_test_x.tolist()[selected_id:selected_id + 1],
                                config.get('dataset', 'malware_dir'),
                                adj_mod=None,
