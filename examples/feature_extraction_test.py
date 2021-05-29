9654d07690ad727886d5e652d30bc24f5cad511e
dli <lideqiang@njust.edu.cn>
2021-05-29 10:33:12 +0800

    update

--- examples/feature_extraction_test.py
+++ examples/feature_extraction_test.py
@@ -43,8 +43,8 @@ def _main():
                                    proc_number=args.proc_number)
     malware_features = feature_extractor.feature_extraction(malware_dir_name)
     print('The number of malware files: ', len(malware_features))
-    benign_features = feature_extractor.feature_extraction(benware_dir_name)
-    print('The number of benign files: ', len(benign_features))
+    # benign_features = feature_extractor.feature_extraction(benware_dir_name)
+    # print('The number of benign files: ', len(benign_features))
 
 
 
