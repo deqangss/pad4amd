016e0739413dadd1adc2bb14df8886a4420f3893
dli <lideqiang@njust.edu.cn>
2021-06-03 10:50:44 +0800

    update

--- core/droidfeature/inverse_feature_extraction.py
+++ core/droidfeature/inverse_feature_extraction.py
@@ -230,7 +230,7 @@ class InverseDroidFeature(object):
                         remove_api(api_name, cg, dst_file)
                     else:
                         # A large scale of insertion operations will trigger unexpected issues, such as method limitation in a class
-                        print('before insert: ', root_call)
+                        print('before insert: ', root_call[0])
                         insert_api(api_name, root_call, dst_file)
             dst_file_apk = os.path.join(save_dir, os.path.splitext(os.path.basename(app_path))[0] + '_adv')
             cmd_response = subprocess.call("apktool -q b " + dst_file + " -o " + dst_file_apk, shell=True)
@@ -406,7 +406,7 @@ def insert_api(api_name, root_call, disassemble_dir):
         try:
             root_class_name, caller_method_statement = rc.split(';', 1)
         except Exception as e:
-            print('root call: ', root_call)
+            print('root call: ', rc)
             print(disassemble_dir)
             raise Exception(e)
         method_match = re.match(
