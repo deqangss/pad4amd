2498f140ff4c408dfcdd1bb1c35780bcee858647
dli <lideqiang@njust.edu.cn>
2021-06-03 09:58:58 +0800

    update

--- core/droidfeature/inverse_feature_extraction.py
+++ core/droidfeature/inverse_feature_extraction.py
@@ -402,7 +402,11 @@ def insert_api(api_name, root_call, disassemble_dir):
 
     injection_done = False
     for rc in root_call:
-        root_class_name, caller_method_statement = rc.split(';', 1)
+        try:
+            root_class_name, caller_method_statement = rc.split(';', 1)
+        except Exception as e:
+            print('root call: ', rc)
+            raise Exception(e)
         method_match = re.match(
             r'^([ ]*?)\.method\s+(?P<methodPre>([^ ].*?))\((?P<methodArg>(.*?))\)(?P<methodRtn>(.*?))$',
             caller_method_statement)
