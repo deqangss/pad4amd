b76d79501a4c33ba56f40b946bcdc39f2579c200
dli <lideqiang@njust.edu.cn>
2021-07-17 09:53:46 +0800

    add professor's idea

--- core/defense/advdet_gmm.py
+++ core/defense/advdet_gmm.py
@@ -90,8 +90,7 @@ class MalwareDetectorIndicator(MalwareDetector, DensityEstimator):
             # filter out examples with low likelihood
             # y_pred = y_pred[indicator_flag]
             # y_true = y_true[indicator_flag]
-            # flag_of_retaining = indicator_flag | (y_pred == 1.)
-            flag_of_retaining = indicator_flag | (y_true == 1.)
+            flag_of_retaining = indicator_flag | (y_pred == 1.) # excluding the examples with ``not sure'' response
             y_pred = y_pred[flag_of_retaining]
             y_true = y_true[flag_of_retaining]
         else:
