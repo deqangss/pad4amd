8affdbee7602541ca4f1bed21563461c39a6bd0f
dli <lideqiang@njust.edu.cn>
2021-07-21 10:41:52 +0800

    hidden rpst

--- core/defense/maldet.py
+++ core/defense/maldet.py
@@ -112,20 +112,25 @@ class MalwareDetector(nn.Module):
     def inference(self, test_data_producer):
         confidences = []
         gt_labels = []
+        x_hidden_np = []
         self.eval()
         with torch.no_grad():
             for ith in tqdm(range(self.n_sample_times)):
                 conf_batches = []
                 for x, adj, y, ig in test_data_producer:
                     x, adj, y = utils.to_tensor(x, adj, y, self.device)
-                    _2, logits = self.forward(x, adj)
+                    x_hidden, logits = self.forward(x, adj)
                     conf_batches.append(F.softmax(logits, dim=-1))
                     if ith == 0:
                         gt_labels.append(y)
+                        x_hidden_np.append(x_hidden.cpu().numpy())
                 conf_batches = torch.vstack(conf_batches)
                 confidences.append(conf_batches)
         gt_labels = torch.cat(gt_labels, dim=0)
+        x_hidden_np = np.vstack(x_hidden_np)
+        utils.dump_pickle((x_hidden_np, gt_labels.cpu().numpy()), './dnn-gat.npz')
         confidences = torch.mean(torch.stack(confidences).permute([1, 0, 2]), dim=1)
+
         return confidences, gt_labels
 
     def inference_batch_wise(self, x, a, y, use_indicator=None):
