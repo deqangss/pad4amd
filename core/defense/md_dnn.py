0052dfe52c14eb0cf53cd8a2312c8a2df70d94d6
dli <lideqiang@njust.edu.cn>
2022-03-31 09:27:38 +0800

    update

--- core/defense/md_dnn.py
+++ core/defense/md_dnn.py
@@ -61,6 +61,9 @@ class DNNMalwareDetector(nn.Module):
 
         self.model_save_path = path.join(config.get('experiments', 'md_dnn') + '_' + self.name,
                                          'model.pth')
+        logger.info('==========================================model architecture================================')
+        logger.info(self)
+        logger.info('===============================================end==========================================')
 
     def parse_args(self,
                    dense_hidden_units=None,
