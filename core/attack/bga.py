e2e918df529b003eef5673762c472041ffe36ec9
dli <lideqiang@njust.edu.cn>
2022-04-16 11:16:34 +0800

    add bga

---
+++ core/attack/bga.py
@@ -0,0 +1,133 @@
+"""
+@inproceedings{al2018adversarial,
+  title={Adversarial deep learning for robust detection of binary encoded malware},
+  author={Al-Dujaili, Abdullah and Huang, Alex and Hemberg, Erik and O’Reilly, Una-May},
+  booktitle={2018 IEEE Security and Privacy Workshops (SPW)},
+  pages={76--82},
+  year={2018},
+  organization={IEEE}
+}
+"""
+
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+from core.attack.base_attack import BaseAttack
+from tools.utils import get_x0, xor_tensors, or_tensors
+from config import logging, ErrorHandler
+
+logger = logging.getLogger('core.attack.bga')
+logger.addHandler(ErrorHandler)
+
+EXP_OVER_FLOW = 1e-30
+
+
+class BGA(BaseAttack):
+    """
+    Multi-step bit gradient ascent
+
+    Parameters
+    ---------
+    @param is_attacker, Boolean, if ture means the role is the attacker
+    @param oblivion, Boolean, whether know the adversary indicator or not
+    @param kappa, attack confidence on adversary indicator
+    @param manipulation_x, manipulations
+    @param omega, the indices of interdependent apis corresponding to each api
+    @param device, 'cpu' or 'cuda'
+    """
+
+    def __init__(self, is_attacker=True, oblivion=False, kappa=1., manipulation_x=None, omega=None, device=None):
+        super(BGA, self).__init__(is_attacker, oblivion, kappa, manipulation_x, omega, device)
+        self.omega = None  # no interdependent apis if just api insertion is considered
+        self.manipulation_z = None  # all apis are permitted to be insertable
+        self.lambda_ = 1.
+
+    def _perturb(self, model, x, label=None,
+                 highest_score=None,
+                 m=10,
+                 lmda=1.,
+                 use_sample=False):
+        """
+        perturb node feature vectors
+
+        Parameters
+        -----------
+        @param model, a victim model
+        @param x: torch.FloatTensor, feature vectors with shape [batch_size, vocab_dim]
+        @param label: torch.LongTensor, ground truth labels
+        @param m: Integer, maximum number of perturbations, namely the hp k in the paper
+        @param lmda, float, penalty factor for balancing the importance of adversary detector
+        @param use_sample, Boolean, whether use random start point
+        """
+        if x is None or x.shape[0] <= 0:
+            return []
+        # some book-keeping
+        sqrt_m = torch.from_numpy(np.sqrt([x.size()[1]])).float().to(model.device)
+
+        adv_x = x.clone()
+        worst_x = x.detach().clone()
+        if highest_score is None:
+            highest_score = self.get_scores(model, adv_x, label).data
+        model.eval()
+        for t in range(m):
+            if t == 0:
+                adv_x = get_x0(adv_x, rounding_threshold=0.5, is_sample=use_sample)
+
+            var_adv_x = torch.autograd.Variable(adv_x, requires_grad=True)
+            loss, _1 = self.get_loss(model, var_adv_x, label, lmda)
+            grad = torch.autograd.grad(loss.mean(), var_adv_x)[0].data
+
+            # filtering un-considered graphs & positions
+            # grad4insertion = (grad > 0) * grad * (adv_x <= 0.5)
+            # grad4ins_ = grad4insertion.reshape(x.shape[0], -1)
+            # compute the updates
+            x_update = (sqrt_m * (1. - 2. * adv_x) * grad >= torch.norm(
+                grad, 2, 1).unsqueeze(1).expand_as(adv_x)).float()
+            # find the next sample with projection to the feasible set
+            adv_x = xor_tensors(x_update, adv_x)
+            adv_x = or_tensors(adv_x, x)
+
+            # select adv x
+            scores = self.get_scores(model, adv_x, label).data
+            replace_flag = (scores > highest_score)
+            highest_score[replace_flag] = scores[replace_flag]
+            worst_x[replace_flag] = adv_x[replace_flag]
+        return worst_x
+
+    def perturb(self, model, x, label=None,
+                steps=10,
+                min_lambda_=1e-5,
+                max_lambda_=1e5,
+                use_sample=False,
+                base=10.,
+                verbose=False):
+        """
+        enhance attack
+        """
+        assert 0 < min_lambda_ <= max_lambda_
+        model.eval()
+        if hasattr(model, 'forward_g'):
+            self.lambda_ = min_lambda_
+        else:
+            self.lambda_ = max_lambda_
+        adv_x = x.detach().clone().to(torch.double)
+        while self.lambda_ <= max_lambda_:
+            with torch.no_grad():
+                _, done = self.get_loss(model, adv_x, label, self.lambda_)
+                score = self.get_scores(model, adv_x, label)
+            if torch.all(done):
+                break
+            pert_x = self._perturb(model, adv_x[~done], label[~done],
+                                   score[~done],
+                                   steps,
+                                   lmda=self.lambda_,
+                                   use_sample=use_sample
+                                   )
+            adv_x[~done] = pert_x
+            self.lambda_ *= base
+        with torch.no_grad():
+            _, done = self.get_loss(model, adv_x, label, self.lambda_)
+            if verbose:
+                logger.info(f"BGA: attack effectiveness {done.sum().item() / x.size()[0] * 100:.3f}%.")
+        return adv_x
---
+++ examples/bga_test.py
@@ -0,0 +1,153 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import os
+import argparse
+
+import numpy as np
+
+from core.defense import Dataset
+from core.defense import DNNMalwareDetector, KernelDensityEstimation, AdvMalwareDetectorICNN, MaxAdvTraining
+from core.attack import BGA
+from tools import utils
+from config import config, logging, ErrorHandler
+
+logger = logging.getLogger('examples.bca_test')
+logger.addHandler(ErrorHandler)
+
+atta_argparse = argparse.ArgumentParser(description='arguments for bca')
+atta_argparse.add_argument('--steps', type=int, default=100,
+                           help='maximum number of iterations.')
+atta_argparse.add_argument('--oblivion', action='store_true', default=False,
+                           help='whether know the adversary indicator or not.')
+atta_argparse.add_argument('--base', type=float, default=10.,
+                           help='base of a logarithm function.')
+atta_argparse.add_argument('--kappa', type=float, default=1.,
+                           help='attack confidence.')
+atta_argparse.add_argument('--n_sample_times', type=int, default=1,
+                           help='data sampling times when waging attacks')
+atta_argparse.add_argument('--real', action='store_true', default=False,
+                           help='whether produce the perturbed apks.')
+atta_argparse.add_argument('--model', type=str, default='maldet',
+                           choices=['md_dnn', 'kde', 'amd_icnn', 'at_amd_pad', 'mad'],
+                           help="model type, either of 'md_dnn', 'kde', 'amd_icnn', 'at_amd_pad', and 'padvtrain'.")
+atta_argparse.add_argument('--model_name', type=str, default='xxxxxxxx-xxxxxx', help='model timestamp.')
+
+
+def _main():
+    args = atta_argparse.parse_args()
+    if args.model == 'md_dnn':
+        save_dir = config.get('experiments', 'md_dnn') + '_' + args.model_name
+    elif args.model == 'kde':
+        save_dir = config.get('experiments', 'kde') + '_' + args.model_name
+    elif args.model == 'amd_icnn':
+        save_dir = config.get('experiments', 'amd_icnn') + '_' + args.model_name
+    elif args.model == 'at_amd_pad':
+        save_dir = config.get('experiments', 'm_adv_training') + '_' + args.model_name
+    elif args.model == 'padvtrain':
+        save_dir = config.get('experiments', 'p_adv_training') + '_' + args.model_name
+    else:
+        raise TypeError("Expected 'md_dnn', 'kde', 'amd_icnn', 'at_amd_pad', and 'padvtrain'.")
+
+    hp_params = utils.read_pickle(os.path.join(save_dir, 'hparam.pkl'))
+    dataset = Dataset(use_cache=hp_params['cache'],
+                      feature_ext_args={'proc_number': hp_params['proc_number']})
+    test_x, testy = dataset.test_dataset
+    mal_save_path = os.path.join(config.get('dataset', 'dataset_dir'), 'attack.idx')
+    if not os.path.exists(mal_save_path):
+        mal_test_x, mal_testy = test_x[testy == 1], testy[testy == 1]
+        utils.dump_pickle_frd_space((mal_test_x, mal_testy), mal_save_path)
+    else:
+        mal_test_x, mal_testy = utils.read_pickle_frd_space(mal_save_path)
+    mal_count = len(mal_testy)
+    if mal_count <= 0:
+        return
+    mal_test_dataset_producer = dataset.get_input_producer(mal_test_x, mal_testy,
+                                                           batch_size=hp_params['batch_size'],
+                                                           name='test')
+    assert dataset.n_classes == 2
+
+    # test
+    if not hp_params['cuda']:
+        dv = 'cpu'
+    else:
+        dv = 'cuda'
+    model = DNNMalwareDetector(dataset.vocab_size,
+                               dataset.n_classes,
+                               device=dv,
+                               name=args.model_name,
+                               **hp_params
+                               )
+    if not(args.model == 'md_dnn' or args.model == 'kde'):
+        if args.model == 'at_amd_pad' and hp_params['detector'] == 'none':
+            pass
+        else:
+            model = AdvMalwareDetectorICNN(model,
+                                           input_size=dataset.vocab_size,
+                                           n_classes=dataset.n_classes,
+                                           device=dv,
+                                           sample_weights=dataset.sample_weights,
+                                           name=args.model_name,
+                                           **hp_params
+                                           )
+    model = model.to(dv).double()
+
+    if args.model == 'kde':
+        model = KernelDensityEstimation(model,
+                                        n_centers=hp_params['n_centers'],
+                                        bandwidth=hp_params['bandwidth'],
+                                        n_classes=dataset.n_classes,
+                                        ratio=hp_params['ratio']
+                                        )
+        model.load()
+    elif args.model == 'at_amd_pad':
+        adv_model = MaxAdvTraining(model)
+        adv_model.load()
+        model = adv_model.model
+    else:
+        model.load()
+
+    logger.info("Load model parameters from {}.".format(model.model_save_path))
+    model.predict(mal_test_dataset_producer, indicator_masking=True)
+
+    attack = BGA(oblivion=args.oblivion,
+                 kappa=args.kappa,
+                 device=model.device)
+
+    logger.info("\nThe maximum number of iterations for each example is {}:".format(args.steps))
+    y_cent_list, x_density_list = [], []
+    x_mod_integrated = []
+    model.eval()
+    for x, y in mal_test_dataset_producer:
+        x, y = utils.to_tensor(x.double(), y.long(), model.device)
+        adv_x_batch = attack.perturb(model, x, y,
+                                     args.steps,
+                                     min_lambda_=1e-5,
+                                     max_lambda_=1e5,
+                                     use_sample=False,
+                                     verbose=True)
+        y_cent_batch, x_density_batch = model.inference_batch_wise(adv_x_batch, y)
+        y_cent_list.append(y_cent_batch)
+        x_density_list.append(x_density_batch)
+        x_mod_integrated.append((adv_x_batch - x).detach().cpu().numpy())
+    y_pred = np.argmax(np.concatenate(y_cent_list), axis=-1)
+    logger.info(
+        f'The mean accuracy on adversarial malware is {sum(y_pred == 1.) / mal_count * 100:.3f}%')
+    if 'indicator' in type(model).__dict__.keys():
+        indicator_flag = model.indicator(np.concatenate(x_density_list))
+        logger.info(f"The effectiveness of indicator is {sum(~indicator_flag) / mal_count * 100:.3f}%")
+        acc_w_indicator = (sum(~indicator_flag) + sum((y_pred == 1.) & indicator_flag)) / mal_count * 100
+        logger.info(f'The mean accuracy on adversarial malware (w/ indicator) is {acc_w_indicator:.3f}%.')
+
+    save_dir = os.path.join(config.get('experiments', 'bga'), args.model)
+    x_mod_integrated = np.concatenate(x_mod_integrated, axis=0)
+    utils.dump_pickle_frd_space(x_mod_integrated,
+                                os.path.join(save_dir, 'x_mod.list'))
+
+    if args.real:
+        attack.produce_adv_mal(x_mod_integrated, mal_test_x.tolist(),
+                               config.get('dataset', 'malware_dir'))
+
+
+if __name__ == '__main__':
+    _main()
