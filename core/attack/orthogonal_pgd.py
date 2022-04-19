783a494042026c0c9be8223bb4b50c3657fccf0a
dli <lideqiang@njust.edu.cn>
2022-04-19 15:53:27 +0800

    update

--- core/attack/orthogonal_pgd.py
+++ core/attack/orthogonal_pgd.py
@@ -148,7 +148,7 @@ class OrthogonalPGD(PGD):
                     torch.tensor(1., dtype=x.dtype, device=x.device),
                     grad / l2norm
                 )
-                # perturbation = torch.where(torch.isnan(perturbation), 0., perturbation)
+                perturbation = torch.where(torch.isnan(perturbation), 0., perturbation)
                 # if self.is_attacker:
                 #     min_val = torch.amin(perturbation, dim=-1, keepdim=True).clamp_(max=0.)
                 #     perturbation += (torch.any(perturbation[:, self.api_flag] < 0, dim=-1,
