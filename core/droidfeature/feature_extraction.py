f0cf76e66de9af85b42be1d7ac15fc0a2091a531
dli <lideqiang@njust.edu.cn>
2022-05-18 18:22:48 +0800

    update

--- core/droidfeature/feature_extraction.py
+++ core/droidfeature/feature_extraction.py
@@ -20,7 +20,7 @@ class Apk2features(object):
                  naive_data_save_dir,
                  intermediate_save_dir,
                  number_of_smali_files=1000000,
-                 max_vocab_size=10000,
+                 max_vocab_size=20000,
                  use_top_disc_features=True,
                  file_ext='.feat',
                  update=False,
@@ -163,16 +163,10 @@ class Apk2features(object):
         ben_feature_frequency[ben_feature_frequency == None] = 0
         ben_feature_frequency /= float(len(gt_labels) - np.sum(gt_labels))
         feature_freq_diff = mal_feature_frequency - ben_feature_frequency
-        print(len(feature_freq_diff))
         feature_freq_diff = feature_freq_diff[feature_freq_diff > 0]  # select the features that benefit to classifying malicious samples
-        print(len(feature_freq_diff))
         posi_selected = np.argsort(feature_freq_diff)[::-1]
         ordered_words = selected_words + [all_words[p] for p in posi_selected]
         selected_words = ordered_words[:maximum_vocab_size]
-        print(len(selected_words))
-        print(selected_words)
-        import sys
-        sys.exit(1)
         selected_word_type = list(map(feat_type_dict.get, selected_words))
         corresponding_word_info = list(map(feat_info_dict.get, selected_words))
         # saving
