2fbc78db0ab813d7b464c592322779f4d1641b38
dli <lideqiang@njust.edu.cn>
2022-05-19 10:15:33 +0800

    update

--- core/droidfeature/feature_extraction.py
+++ core/droidfeature/feature_extraction.py
@@ -163,7 +163,13 @@ class Apk2features(object):
         ben_feature_frequency[ben_feature_frequency == None] = 0
         ben_feature_frequency /= float(len(gt_labels) - np.sum(gt_labels))
         feature_freq_diff = mal_feature_frequency - ben_feature_frequency
-        feature_freq_diff = feature_freq_diff[feature_freq_diff > 0]  # select the features that benefit to classifying malicious samples
+        # select the features that benefit to classifying malicious samples
+        # this increases the FPR
+        print(len(feature_freq_diff))
+        feature_freq_diff = feature_freq_diff[feature_freq_diff >= 0]
+        print(len(feature_freq_diff))
+        import sys
+        sys.exit(1)
         posi_selected = np.argsort(feature_freq_diff)[::-1]
         ordered_words = selected_words + [all_words[p] for p in posi_selected]
         selected_words = ordered_words[:maximum_vocab_size]
