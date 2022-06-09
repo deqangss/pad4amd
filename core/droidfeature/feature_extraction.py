8fe8a95675a056ddb774c0084bdcdc51e3f16cea
dli <lideqiang@njust.edu.cn>
2022-06-09 23:40:58 +0800

    update

--- core/droidfeature/feature_extraction.py
+++ core/droidfeature/feature_extraction.py
@@ -157,9 +157,14 @@ class Apk2features(object):
         ben_feature_frequency = np.array(list(map(counter_ben.get, all_words)))
         ben_feature_frequency[ben_feature_frequency == None] = 0
         ben_feature_frequency /= float(len(gt_labels) - np.sum(gt_labels))
-        feature_freq_diff = abs(mal_feature_frequency - ben_feature_frequency)
-        posi_selected = np.argsort(feature_freq_diff)[::-1]
+        feature_freq_diff = (mal_feature_frequency - ben_feature_frequency) >= 0
+        posi_selected = np.argsort(mal_feature_frequency - ben_feature_frequency)[::-1]
         ordered_words = selected_words + [all_words[p] for p in posi_selected]
+        print(len(ordered_words))
+        ordered_words = selected_words + [all_words[p] for p, flag in enumerate(feature_freq_diff) if flag]
+        print(len(ordered_words))
+        import sys
+        sys.exit(1)
         selected_words = ordered_words[:maximum_vocab_size]
         selected_word_type = list(map(feat_type_dict.get, selected_words))
         corresponding_word_info = list(map(feat_info_dict.get, selected_words))
