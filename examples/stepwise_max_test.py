78b2b65b427aeddcc0aec9170db04969e11cfc43
dli <lideqiang@njust.edu.cn>
2022-07-02 17:28:26 +0800

    update

--- examples/stepwise_max_test.py
+++ examples/stepwise_max_test.py
@@ -191,10 +191,6 @@ def _main():
     if not os.path.exists(save_dir):
         utils.mkdir(save_dir)
     x_mod_integrated = np.concatenate(x_mod_integrated, axis=0)
-    success_flag = (y_pred == 0.) & indicator_flag
-    print(success_flag)
-    print(np.sum(success_flag))
-    print(np.sum(np.abs(x_mod_integrated[success_flag]), axis=-1))
     utils.dump_pickle_frd_space(x_mod_integrated,
                                 os.path.join(save_dir, 'x_mod.list'))
 
