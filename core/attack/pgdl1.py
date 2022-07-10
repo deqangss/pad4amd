814d70459257cf642f7229cca990ec3a70ff56c3
dli <lideqiang@njust.edu.cn>
2022-07-10 11:33:48 +0800

    update

--- core/attack/pgdl1.py
+++ core/attack/pgdl1.py
@@ -39,7 +39,7 @@ class PGDl1(BaseAttack):
         self.lambda_ = 1.
 
     def _perturb(self, model, x,  label=None,
-                 m_perturbations=10,
+                 steps=10,
                  lambda_=1.):
         """
         perturb node feature vectors
@@ -49,18 +49,20 @@ class PGDl1(BaseAttack):
         @param model, a victim model
         @param x: torch.FloatTensor, node feature vectors (each represents the occurrences of apis in a graph) with shape [batch_size, number_of_graphs, vocab_dim]
         @param label: torch.LongTensor, ground truth labels
-        @param m_perturbations: Integer, maximum number of perturbations
+        @param steps: Integer, maximum number of perturbations
         @param lambda_, float, penalty factor
         """
         if x is None or x.shape[0] <= 0:
             return []
         adv_x = x
+        worst_x = x.detach().clone()
         self.lambda_ = lambda_
         self.padding_mask = torch.sum(adv_x, dim=-1, keepdim=True) > 1  # we set a graph contains two apis at least
         model.eval()
-        for t in range(m_perturbations):
+        for t in range(steps):
             var_adv_x = torch.autograd.Variable(adv_x, requires_grad=True)
             loss, done = self.get_loss(model, var_adv_x, label, self.lambda_)
+            worst_x[done] = adv_x[done]
             if torch.all(done):
                 break
             grad = torch.autograd.grad(torch.mean(loss), var_adv_x)[0]
@@ -68,7 +70,9 @@ class PGDl1(BaseAttack):
             # avoid to perturb the examples that are successful to evade the victim
             perturbation[done] = 0.
             adv_x = torch.clamp(adv_x + perturbation * direction, min=0., max=1.)
-        return adv_x
+        done = self.get_scores(model, adv_x, label)
+        worst_x[done] = adv_x[done]
+        return worst_x
 
     def perturb(self, model, x, label=None,
                 steps=10,
