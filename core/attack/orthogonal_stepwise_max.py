e4d38a239927535226855ec90eeb34bebf1b9e2e
dli <lideqiang@njust.edu.cn>
2022-07-10 19:29:18 +0800

    update

--- core/attack/orthogonal_stepwise_max.py
+++ core/attack/orthogonal_stepwise_max.py
@@ -86,7 +86,9 @@ class OrthogonalStepwiseMax(StepwiseMax):
                 n_attacks = len(pertb_x_list)
                 pertbx = torch.vstack(pertb_x_list)
                 label_ext = torch.cat([label[~done]] * n_attacks)
-                scores, _1 = self.get_scores(model, pertbx, label_ext)
+                scores, _done = self.get_scores(model, pertbx, label_ext)
+                max_v = scores.amax() if scores.amax() > 0 else 100.
+                scores[_done] += max_v
                 pertbx = pertbx.reshape(n_attacks, num_sample_red, *red_n).permute([1, 0, *red_ind])
                 scores = scores.reshape(n_attacks, num_sample_red).permute(1, 0)
                 _, s_idx = scores.max(dim=-1)
