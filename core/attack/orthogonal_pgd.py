51d0bb0610b6e7a48aed50c8df7eec3643471ac9
dli <lideqiang@njust.edu.cn>
2022-07-20 15:23:23 +0800

    update

--- core/attack/orthogonal_pgd.py
+++ core/attack/orthogonal_pgd.py
@@ -112,7 +112,7 @@ class OrthogonalPGD(PGD):
             # logits_classifier[range(batch_size), 0] = logits_classifier[range(batch_size), 0]
             # has_attack_succeeded = (logits_classifier.argmax(1) == 0.)[:, None].float()
             disc_logits_classifier, _1 = model.forward(round_x(adv_x))
-            disc_logits_classifier[range(batch_size), 0] = disc_logits_classifier[range(batch_size), 0] - 20.
+            disc_logits_classifier[range(batch_size), 0] = disc_logits_classifier[range(batch_size), 0]
             has_attack_succeeded = (disc_logits_classifier.argmax(1) == 0.)[:, None].float()  # customized label
 
             if self.k:
