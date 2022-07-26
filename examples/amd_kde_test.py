d049cadb2da9ce490dd7a042b69e704a8a94510a
dli <lideqiang@njust.edu.cn>
2022-07-26 20:20:48 +0800

    update

--- examples/amd_kde_test.py
+++ examples/amd_kde_test.py
@@ -68,7 +68,7 @@ def _main():
 
     kde.load()
     kde.get_threshold(val_dataset_producer, ratio=args.ratio)
-    kde.predict(test_dataset_producer, indicator_masking=False)
+    kde.predict(test_dataset_producer)
 
 
 if __name__ == '__main__':
