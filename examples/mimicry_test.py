5bcd48124a2b6f5fcf38a4d32e0df2f588319a1b
dli <lideqiang@njust.edu.cn>
2022-08-01 09:46:04 +0800

    update

--- examples/mimicry_test.py
+++ examples/mimicry_test.py
@@ -186,7 +186,7 @@ def _main():
 
         # x_mod_list = utils.read_pickle_frd_space(os.path.join(save_dir, 'x_mod.list'))
 
-        attack.produce_adv_mal(x_mod_list[:100], mal_test_x.tolist()[:100],
+        attack.produce_adv_mal(x_mod_list, mal_test_x.tolist(),
                                config.get('dataset', 'malware_dir'),
                                save_dir=adv_app_dir)
         adv_feature_paths = dataset.apk_preprocess(adv_app_dir, update_feature_extraction=False)
